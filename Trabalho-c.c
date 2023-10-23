e6ff26feaecc838dbcb5fac0504ba537882fe8ae
João Pedro <jn459998@gmail.com>
2023-10-23 18:29:29 -0300

    Adição de arquivos

---
+++ Trabalho-c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Estrutura de dados
+struct log
+{
+    char nome[50];
+    int idade;
+    char telefone[15];
+    char endereco[100]
+};
+
+// função para adicionar um novo registro
+void adicionarRegistro(struct log *registros, int *quantidade);
+
+int main()
+{
+    printf("%d", quantidade);
+    return 0;
+}
